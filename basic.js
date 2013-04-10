1edf8e994565cea2fc23e61e49d247c83b134875
miyano <t.miyano@gmail.com>
2013-04-10 16:47:48 +0900

    update nangyou

--- basic.js
+++ basic.js
@@ -2,8 +2,37 @@ function assert(err) {
     alert(err);
 }
 
-function get_node_textContent(node) {
+function get_text_content(node) {
     return node.textContent ||node.text;
+}
+
+function remove_all_child(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
+
+function textNodeToHTML(node) {
+    var html = '';
+    var child = node.firstChild;
+    while (child) {
+        //var debug = 'type: ' + child.nodeType + '\nvalue: ' + child.nodeValue + '\ntagName: ' + child.tagName;
+        //alert(debug);
+        switch (child.nodeType) {
+        case child.TEXT_NODE:
+            html += child.nodeValue;
+            break;
+        case child.ELEMENT_NODE:
+            if (child.tagName == 'br') {
+                html += '<br>';
+            }
+            break;
+        default:
+            break;
+        }
+        child = child.nextSibling;
+    }
+    return html;
 }
 
 /*
@@ -13,4 +42,12 @@ function get_node_textContent(node) {
 */
 function get_selected_option_value(select) {
   return select.options.item(select.selectedIndex).value;
+}
+
+function debug_print_object(object) {
+    var text = 'object: ' + object + "<br>";
+    for (var i in object) {
+        text += '&nbsp;&nbsp;&nbsp;' + i + ': ' + object[i] + "<br>";
+    }
+    return text;
 }
